2055507a04cd88b4d28977444b14b470340a4188
dumuchenglin123 <13971352275@139.com>
2017-11-08 15:47:37 +0800

    再次修改 cl

--- src/etGrid/plugins/menu.js
+++ src/etGrid/plugins/menu.js
@@ -69,4 +69,42 @@
             }
         });
     };
+    // eg:
+    /* gridObj.toolbar = {
+        items: [
+            {
+                type: "menu",
+                label: '添的方式的加',
+                //width: 200,   // 设置菜单宽度
+                id: 'menu',     // 必填参数
+                items: [{
+                    text: '文件',
+                    children: [{   // 二级菜单
+                        text: 'El',
+                        children: [{
+                            text: 'kjkll',
+                            click: itemclick
+                        }]
+                    },
+                    {
+                        text: 'Word',
+                    },
+                    {
+                        text: 'PDF',
+                    },
+                    {
+                        text: 'TXT',
+                    },
+                    {
+                        line: true
+                    },
+                    {
+                        text: 'XML',
+                    }
+                    ]
+                }, {
+                    line: true
+                }]
+            }]
+    } */
 }(jQuery));
